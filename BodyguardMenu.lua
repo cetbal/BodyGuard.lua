5c53aaed271be7e0d3eaea5eb01827065c2afcdd
cetbal <53950298+cetbal@users.noreply.github.com>
2026-03-13 20:22:18 +0100

    Update BodyguardMenu.lua

--- BodyguardMenu.lua
+++ BodyguardMenu.lua
@@ -1,26 +1,26 @@
 dofile("C:/Users/GarnalG/Documents/Cherax/Lua/DannyScript/Natives/natives.lua")
 
 local bodyguards = {}
-local playerGroup = 0
+local groupId = 0
 
 ------------------------------------------------
 -- MODELS
 ------------------------------------------------
 
 local MODELS = {
-    "s_m_m_security_01",
-    "s_m_m_fiboffice_01",
-    "s_m_m_ciasec_01",
-    "s_m_y_swat_01",
-    "s_m_y_blackops_01"
+"s_m_m_security_01",
+"s_m_m_fiboffice_01",
+"s_m_m_ciasec_01",
+"s_m_y_swat_01",
+"s_m_y_blackops_01"
 }
 
 local MODEL_NAMES = {
-    "Security",
-    "FIB",
-    "CIA",
-    "SWAT",
-    "Black Ops"
+"Security",
+"FIB",
+"CIA",
+"SWAT",
+"Black Ops"
 }
 
 ------------------------------------------------
@@ -28,19 +28,19 @@ local MODEL_NAMES = {
 ------------------------------------------------
 
 local WEAPONS = {
-    "WEAPON_PISTOL",
-    "WEAPON_COMBATPISTOL",
-    "WEAPON_SMG",
-    "WEAPON_CARBINERIFLE",
-    "WEAPON_PUMPSHOTGUN"
+"WEAPON_PISTOL",
+"WEAPON_COMBATPISTOL",
+"WEAPON_SMG",
+"WEAPON_CARBINERIFLE",
+"WEAPON_PUMPSHOTGUN"
 }
 
 local WEAPON_NAMES = {
-    "Pistol",
-    "Combat Pistol",
-    "SMG",
-    "Carbine Rifle",
-    "Shotgun"
+"Pistol",
+"Combat Pistol",
+"SMG",
+"Carbine Rifle",
+"Shotgun"
 }
 
 ------------------------------------------------
@@ -50,49 +50,44 @@ local WEAPON_NAMES = {
 local currentModel = 1
 local currentWeapon = 4
 local spawnAmount = 3
+local followDistance = 3
 
 ------------------------------------------------
--- HELPERS
+-- GROUP
 ------------------------------------------------
-
-local function info(text)
-    Logger.Log(eLogColor.LIGHTGREEN, "Bodyguard", text)
-end
 
 local function ensure_group()
 
-    if playerGroup ~= 0 then
-        return
-    end
+if groupId ~= 0 then return end
 
-    playerGroup = PED.CREATE_GROUP(0)
+groupId = PED.CREATE_GROUP(0)
 
-    PED.SET_PED_AS_GROUP_LEADER(
-        PLAYER.PLAYER_PED_ID(),
-        playerGroup
-    )
+PED.SET_PED_AS_GROUP_LEADER(
+PLAYER.PLAYER_PED_ID(),
+groupId
+)
 
-    PED.SET_GROUP_SEPARATION_RANGE(
-        playerGroup,
-        9999.0
-    )
+PED.SET_GROUP_SEPARATION_RANGE(
+groupId,
+9999.0
+)
 
 end
 
-local function add_to_group(ped)
+local function add_guard(ped)
 
-    ensure_group()
+ensure_group()
 
-    PED.SET_PED_AS_GROUP_MEMBER(
-        ped,
-        playerGroup
-    )
+PED.SET_PED_AS_GROUP_MEMBER(
+ped,
+groupId
+)
 
-    PED.SET_PED_CAN_TELEPORT_TO_GROUP_LEADER(
-        ped,
-        playerGroup,
-        true
-    )
+PED.SET_PED_CAN_TELEPORT_TO_GROUP_LEADER(
+ped,
+groupId,
+true
+)
 
 end
 
@@ -102,75 +97,78 @@ end
 
 local function spawn_guard()
 
-    local playerPed = PLAYER.PLAYER_PED_ID()
+local playerPed = PLAYER.PLAYER_PED_ID()
 
-    local coords = ENTITY.GET_ENTITY_COORDS(
-        playerPed,
-        true
-    )
+local coords = ENTITY.GET_ENTITY_COORDS(playerPed,true)
 
-    local model = MODELS[currentModel]
+local model = MODELS[currentModel]
 
-    local hash = MISC.GET_HASH_KEY(model)
+local hash = MISC.GET_HASH_KEY(model)
 
-    STREAMING.REQUEST_MODEL(hash)
+STREAMING.REQUEST_MODEL(hash)
 
-    local i = 0
-    while not STREAMING.HAS_MODEL_LOADED(hash) and i < 200 do
-        SYSTEM.WAIT(0)
-        i = i + 1
-    end
+local i=0
+while not STREAMING.HAS_MODEL_LOADED(hash) and i < 200 do
+SYSTEM.WAIT(0)
+i=i+1
+end
 
-    local ped = PED.CREATE_PED(
-        4,
-        hash,
-        coords.x + math.random(-3,3),
-        coords.y + math.random(-3,3),
-        coords.z,
-        0.0,
-        true,
-        true
-    )
+local ped = PED.CREATE_PED(
+4,
+hash,
+coords.x + math.random(-3,3),
+coords.y + math.random(-3,3),
+coords.z,
+0.0,
+true,
+true
+)
 
-    if ped == 0 then
-        return
-    end
+if ped == 0 then return end
 
-    ------------------------------------------------
-    -- WEAPON
-    ------------------------------------------------
+------------------------------------------------
+-- weapon
+------------------------------------------------
 
-    local weapon = MISC.GET_HASH_KEY(
-        WEAPONS[currentWeapon]
-    )
+local weapon = MISC.GET_HASH_KEY(WEAPONS[currentWeapon])
 
-    WEAPON.GIVE_WEAPON_TO_PED(
-        ped,
-        weapon,
-        9999,
-        false,
-        true
-    )
+WEAPON.GIVE_WEAPON_TO_PED(
+ped,
+weapon,
+9999,
+false,
+true
+)
 
-    ------------------------------------------------
-    -- STATS
-    ------------------------------------------------
+------------------------------------------------
+-- stats
+------------------------------------------------
 
-    PED.SET_PED_ACCURACY(ped, 80)
-    PED.SET_PED_ARMOUR(ped, 100)
+PED.SET_PED_ACCURACY(ped,80)
+PED.SET_PED_ARMOUR(ped,100)
 
-    PED.SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(
-        ped,
-        true
-    )
+PED.SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(
+ped,
+true
+)
 
-    ------------------------------------------------
-    -- GROUP
-    ------------------------------------------------
+ENTITY.SET_ENTITY_AS_MISSION_ENTITY(ped,true,true)
 
-    add_to_group(ped)
+add_guard(ped)
 
-    table.insert(bodyguards, ped)
+table.insert(bodyguards,ped)
+
+end
+
+------------------------------------------------
+-- SPAWN MULTI
+------------------------------------------------
+
+local function spawn_multiple(amount)
+
+for i=1,amount do
+spawn_guard()
+end
 
 end
 
@@ -180,49 +178,96 @@ end
 
 local function order_follow()
 
-    local playerPed = PLAYER.PLAYER_PED_ID()
+local playerPed = PLAYER.PLAYER_PED_ID()
 
-    for i,ped in ipairs(bodyguards) do
+for i,ped in ipairs(bodyguards) do
 
-        if ENTITY.DOES_ENTITY_EXIST(ped) then
+if ENTITY.DOES_ENTITY_EXIST(ped) then
 
-            TASK.TASK_FOLLOW_TO_OFFSET_OF_ENTITY(
-                ped,
-                playerPed,
-                math.random(-2,2),
-                math.random(-2,2),
-                0.0,
-                3.0,
-                -1,
-                2.0,
-                true
-            )
+TASK.TASK_FOLLOW_TO_OFFSET_OF_ENTITY(
+ped,
+playerPed,
+math.random(-followDistance,followDistance),
+math.random(-followDistance,followDistance),
+0,
+3.0,
+-1,
+2.0,
+true
+)
 
-        end
+end
 
-    end
+end
 
 end
 
 ------------------------------------------------
--- AGGRESSIVE
+-- ATTACK AROUND
 ------------------------------------------------
 
-local function order_attack()
+local function attack_mode()
 
-    for i,ped in ipairs(bodyguards) do
+for i,ped in ipairs(bodyguards) do
 
-        if ENTITY.DOES_ENTITY_EXIST(ped) then
+if ENTITY.DOES_ENTITY_EXIST(ped) then
 
-            TASK.TASK_COMBAT_HATED_TARGETS_AROUND_PED(
-                ped,
-                100.0,
-                0
-            )
+TASK.TASK_COMBAT_HATED_TARGETS_AROUND_PED(
+ped,
+120.0,
+0
+)
 
-        end
+end
 
-    end
+end
+
+end
+
+------------------------------------------------
+-- VEHICLE
+------------------------------------------------
+
+local function seat_vehicle()
+
+local veh = PED.GET_VEHICLE_PED_IS_IN(
+PLAYER.PLAYER_PED_ID(),
+false
+)
+
+if veh == 0 then return end
+
+local seat = -1
+
+for i,ped in ipairs(bodyguards) do
+
+PED.SET_PED_INTO_VEHICLE(
+ped,
+veh,
+seat
+)
+
+seat = seat + 1
+
+end
+
+end
+
+local function exit_vehicle()
+
+for i,ped in ipairs(bodyguards) do
+
+if PED.IS_PED_IN_ANY_VEHICLE(ped,false) then
+
+TASK.TASK_LEAVE_VEHICLE(
+ped,
+PED.GET_VEHICLE_PED_IS_IN(ped,false),
+0
+)
+
+end
+
+end
 
 end
 
@@ -232,20 +277,17 @@ end
 
 local function delete_all()
 
-    for i,ped in ipairs(bodyguards) do
+for i,ped in ipairs(bodyguards) do
 
-        if ENTITY.DOES_ENTITY_EXIST(ped) then
+if ENTITY.DOES_ENTITY_EXIST(ped) then
 
-            ENTITY.SET_ENTITY_HEALTH(
-                ped,
-                0
-            )
+ENTITY.SET_ENTITY_HEALTH(ped,0)
 
-        end
+end
 
-    end
+end
 
-    bodyguards = {}
+bodyguards = {}
 
 end
 
@@ -253,64 +295,54 @@ end
 -- FEATURES
 ------------------------------------------------
 
-FeatureMgr.AddFeature(
-Utils.Joaat("BG_Spawn"),
-"Spawn Bodyguard",
-eFeatureType.Button,
-"",
-function()
+FeatureMgr.AddFeature(Utils.Joaat("BGV10_Spawn"),"Spawn Bodyguard",eFeatureType.Button,"",function()
 
-    spawn_guard()
-
-    order_follow()
+spawn_guard()
+order_follow()
 
 end)
 
-FeatureMgr.AddFeature(
-Utils.Joaat("BG_Spawn5"),
-"Spawn 5",
-eFeatureType.Button,
-"",
-function()
+FeatureMgr.AddFeature(Utils.Joaat("BGV10_Spawn5"),"Spawn 5",eFeatureType.Button,"",function()
 
-    for i=1,5 do
-        spawn_guard()
-    end
-
-    order_follow()
+spawn_multiple(5)
+order_follow()
 
 end)
 
-FeatureMgr.AddFeature(
-Utils.Joaat("BG_Follow"),
-"Follow Player",
-eFeatureType.Button,
-"",
-function()
+FeatureMgr.AddFeature(Utils.Joaat("BGV10_Spawn10"),"Spawn 10",eFeatureType.Button,"",function()
 
-    order_follow()
+spawn_multiple(10)
+order_follow()
 
 end)
 
-FeatureMgr.AddFeature(
-Utils.Joaat("BG_Attack"),
-"Aggressive Mode",
-eFeatureType.Button,
-"",
-function()
+FeatureMgr.AddFeature(Utils.Joaat("BGV10_Follow"),"Follow Player",eFeatureType.Button,"",function()
 
-    order_attack()
+order_follow()
 
 end)
 
-FeatureMgr.AddFeature(
-Utils.Joaat("BG_Delete"),
-"Delete All",
-eFeatureType.Button,
-"",
-function()
+FeatureMgr.AddFeature(Utils.Joaat("BGV10_Attack"),"Aggressive Mode",eFeatureType.Button,"",function()
 
-    delete_all()
+attack_mode()
+
+end)
+
+FeatureMgr.AddFeature(Utils.Joaat("BGV10_Seat"),"Seat In Vehicle",eFeatureType.Button,"",function()
+
+seat_vehicle()
+
+end)
+
+FeatureMgr.AddFeature(Utils.Joaat("BGV10_Exit"),"Exit Vehicle",eFeatureType.Button,"",function()
+
+exit_vehicle()
+
+end)
+
+FeatureMgr.AddFeature(Utils.Joaat("BGV10_Delete"),"Delete All",eFeatureType.Button,"",function()
+
+delete_all()
 
 end)
 
@@ -320,32 +352,42 @@ end)
 
 ClickGUI.AddTab("Bodyguard Menu", function()
 
-    if ClickGUI.BeginCustomChildWindow("Spawn") then
+if ClickGUI.BeginCustomChildWindow("Deployment") then
 
-        ClickGUI.RenderFeature(Utils.Joaat("BG_Spawn"))
-        ClickGUI.RenderFeature(Utils.Joaat("BG_Spawn5"))
+ClickGUI.RenderFeature(Utils.Joaat("BGV10_Spawn"))
+ClickGUI.RenderFeature(Utils.Joaat("BGV10_Spawn5"))
+ClickGUI.RenderFeature(Utils.Joaat("BGV10_Spawn10"))
 
-        ClickGUI.EndCustomChildWindow()
+ClickGUI.EndCustomChildWindow()
 
-    end
+end
 
-    if ClickGUI.BeginCustomChildWindow("Commands") then
+if ClickGUI.BeginCustomChildWindow("AI Commands") then
 
-        ClickGUI.RenderFeature(Utils.Joaat("BG_Follow"))
-        ClickGUI.RenderFeature(Utils.Joaat("BG_Attack"))
+ClickGUI.RenderFeature(Utils.Joaat("BGV10_Follow"))
+ClickGUI.RenderFeature(Utils.Joaat("BGV10_Attack"))
 
-        ClickGUI.EndCustomChildWindow()
+ClickGUI.EndCustomChildWindow()
 
-    end
+end
 
-    if ClickGUI.BeginCustomChildWindow("Cleanup") then
+if ClickGUI.BeginCustomChildWindow("Vehicle") then
 
-        ClickGUI.RenderFeature(Utils.Joaat("BG_Delete"))
+ClickGUI.RenderFeature(Utils.Joaat("BGV10_Seat"))
+ClickGUI.RenderFeature(Utils.Joaat("BGV10_Exit"))
 
-        ClickGUI.EndCustomChildWindow()
+ClickGUI.EndCustomChildWindow()
 
-    end
+end
+
+if ClickGUI.BeginCustomChildWindow("Cleanup") then
+
+ClickGUI.RenderFeature(Utils.Joaat("BGV10_Delete"))
+
+ClickGUI.EndCustomChildWindow()
+
+end
 
 end)
 
-info("Bodyguard V9 Loaded")
+Logger.Log(eLogColor.LIGHTGREEN,"Bodyguard","V10 Loaded")
