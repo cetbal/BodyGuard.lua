69845dacdab60cb1a8ab97e309269c93dd6428bd
cetbal <53950298+cetbal@users.noreply.github.com>
2026-03-14 19:11:55 +0100

    Update BodyguardMenu.lua

--- BodyguardMenu.lua
+++ BodyguardMenu.lua
@@ -1,117 +1,222 @@
--- Bodyguard Menu V15 Stable Cherax
-
-print("[Bodyguard] Script loaded")
+dofile("C:/Users/GarnalG/Documents/Cherax/Lua/DannyScript/Natives/natives.lua")
 
 local bodyguards = {}
 
-local models = {
-    "s_m_m_security_01",
-    "s_m_m_fiboffice_01",
-    "s_m_m_iaa_01",
-    "s_m_m_swat_01"
+------------------------------------------------
+-- OPTIONS
+------------------------------------------------
+
+local MODELS = {
+    {label="Security", model="s_m_m_security_01"},
+    {label="FIB", model="s_m_m_fiboffice_01"},
+    {label="CIA", model="s_m_m_ciasec_01"},
+    {label="SWAT", model="s_m_y_swat_01"},
+    {label="Black Ops", model="s_m_y_blackops_01"}
 }
 
-local weapons = {
-    "WEAPON_PISTOL",
-    "WEAPON_SMG",
-    "WEAPON_CARBINERIFLE"
+local WEAPONS = {
+    {label="Pistol", weapon="WEAPON_PISTOL", ammo=500},
+    {label="Combat Pistol", weapon="WEAPON_COMBATPISTOL", ammo=500},
+    {label="SMG", weapon="WEAPON_SMG", ammo=800},
+    {label="Carbine Rifle", weapon="WEAPON_CARBINERIFLE", ammo=1200}
 }
 
-local modelIndex = 1
-local weaponIndex = 1
+local MODEL_LABELS={}
+local WEAPON_LABELS={}
+
+for i,v in ipairs(MODELS) do MODEL_LABELS[i]=v.label end
+for i,v in ipairs(WEAPONS) do WEAPON_LABELS[i]=v.label end
+
+local currentModel=1
+local currentWeapon=1
+local spawnAmount=3
 
 ------------------------------------------------
--- MENU ROOT
+-- HASHES
 ------------------------------------------------
 
-local root = menu.add_feature("Bodyguard Menu", "parent", 0)
+local HASH_MODEL=Utils.Joaat("BG_Model")
+local HASH_WEAPON=Utils.Joaat("BG_Weapon")
+local HASH_AMOUNT=Utils.Joaat("BG_Amount")
 
 ------------------------------------------------
--- MODEL MENU
+-- UTILS
 ------------------------------------------------
 
-local modelMenu = menu.add_feature("Model", "autoaction_value_str", root.id, function(f)
+local function info(t)
+Logger.Log(eLogColor.LIGHTGREEN,"Bodyguard",t)
+GUI.AddToast("Bodyguard",t,2500,eToastPos.TOP_RIGHT)
+end
 
-    modelIndex = f.value + 1
-    print("Model selected: "..models[modelIndex])
+local function loadModel(hash)
+STREAMING.REQUEST_MODEL(hash)
 
-end)
+local i=0
+while not STREAMING.HAS_MODEL_LOADED(hash) and i<200 do
+SYSTEM.WAIT(0)
+i=i+1
+end
 
-modelMenu:set_str_data(models)
-
-------------------------------------------------
--- WEAPON MENU
-------------------------------------------------
-
-local weaponMenu = menu.add_feature("Weapon", "autoaction_value_str", root.id, function(f)
-
-    weaponIndex = f.value + 1
-    print("Weapon selected: "..weapons[weaponIndex])
-
-end)
-
-weaponMenu:set_str_data(weapons)
+return STREAMING.HAS_MODEL_LOADED(hash)
+end
 
 ------------------------------------------------
--- SPAWN BODYGUARD
+-- SPAWN
 ------------------------------------------------
 
-menu.add_feature("Spawn Bodyguard", "action", root.id, function()
+local function spawnOne(x,y,z)
 
-    local player = player.player_ped()
+local model=MODELS[currentModel]
+local weapon=WEAPONS[currentWeapon]
 
-    local pos = entity.get_entity_coords(player)
+local hash=MISC.GET_HASH_KEY(model.model)
 
-    local model = gameplay.get_hash_key(models[modelIndex])
+if not loadModel(hash) then
+info("Model load failed")
+return
+end
 
-    streaming.request_model(model)
+local ped=PED.CREATE_PED(4,hash,x,y,z,0,false,false)
 
-    while not streaming.has_model_loaded(model) do
-        system.wait(0)
-    end
+WEAPON.GIVE_WEAPON_TO_PED(
+ped,
+MISC.GET_HASH_KEY(weapon.weapon),
+weapon.ammo,
+false,
+true
+)
 
-    local ped = ped.create_ped(
-        26,
-        model,
-        pos.x + math.random(-2,2),
-        pos.y + math.random(-2,2),
-        pos.z,
-        0,
-        true,
-        true
-    )
+PED.SET_PED_AS_GROUP_MEMBER(
+ped,
+PLAYER.GET_PLAYER_GROUP(PLAYER.PLAYER_ID())
+)
 
-    weapon.give_delayed_weapon_to_ped(
-        ped,
-        gameplay.get_hash_key(weapons[weaponIndex]),
-        999,
-        true
-    )
+table.insert(bodyguards,ped)
 
-    table.insert(bodyguards,ped)
+end
 
-    print("[Bodyguard] Spawned")
+local function spawnMultiple()
 
-end)
+local player=PLAYER.PLAYER_PED_ID()
+local pos=ENTITY.GET_ENTITY_COORDS(player,true)
+
+for i=1,spawnAmount do
+spawnOne(
+pos.x+(i*1.2),
+pos.y+1.5,
+pos.z
+)
+end
+
+info(spawnAmount.." bodyguards spawned")
+
+end
 
 ------------------------------------------------
--- DELETE ALL
+-- DELETE
 ------------------------------------------------
 
-menu.add_feature("Delete Bodyguards", "action", root.id, function()
+local function deleteAll()
 
-    for k,ped in pairs(bodyguards) do
+for _,ped in pairs(bodyguards) do
 
-        if entity.does_entity_exist(ped) then
-            entity.delete_entity(ped)
-        end
+if ENTITY.DOES_ENTITY_EXIST(ped) then
+ENTITY.DELETE_ENTITY(ped)
+end
 
-    end
+end
 
-    bodyguards = {}
+bodyguards={}
 
-    print("[Bodyguard] Deleted")
+info("All bodyguards deleted")
 
-end)
+end
 
-print("[Bodyguard] Menu Ready")
+------------------------------------------------
+-- FEATURES
+------------------------------------------------
+
+FeatureMgr.AddFeature(
+HASH_MODEL,
+"Agent Model",
+eFeatureType.Combo,
+"Choose model",
+function(f)
+
+local i=f:GetListIndex()
+currentModel=i+1
+
+info("Model "..MODELS[currentModel].label)
+
+end,
+true
+)
+
+FeatureMgr.GetFeature(HASH_MODEL):SetList(MODEL_LABELS)
+
+------------------------------------------------
+
+FeatureMgr.AddFeature(
+HASH_WEAPON,
+"Primary Weapon",
+eFeatureType.Combo,
+"Choose weapon",
+function(f)
+
+local i=f:GetListIndex()
+currentWeapon=i+1
+
+info("Weapon "..WEAPONS[currentWeapon].label)
+
+end,
+true
+)
+
+FeatureMgr.GetFeature(HASH_WEAPON):SetList(WEAPON_LABELS)
+
+------------------------------------------------
+
+FeatureMgr.AddFeature(
+HASH_AMOUNT,
+"Spawn Amount",
+eFeatureType.SliderInt,
+"Number of guards",
+function(f)
+
+spawnAmount=f:GetIntValue()
+
+end,
+true
+)
+
+------------------------------------------------
+
+FeatureMgr.AddFeature(
+Utils.Joaat("BG_Spawn"),
+"Spawn Bodyguards",
+eFeatureType.Button,
+"",
+function()
+
+spawnMultiple()
+
+end,
+true
+)
+
+------------------------------------------------
+
+FeatureMgr.AddFeature(
+Utils.Joaat("BG_Delete"),
+"Delete All",
+eFeatureType.Button,
+"",
+function()
+
+deleteAll()
+
+end,
+true
+)
+
+info("Bodyguard Menu V15 Loaded")
